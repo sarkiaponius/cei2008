013ca1cc73f345714f6de5b5d5108d406d7e9bb7
sarkiaponius <sarkiaponius@alice.it>
2014-04-07 23:33:27 +0200

    - Mappa libri CEI - libri Sword - Intestazione corretta delle stanze, dopo mappatura libri

--- src/Test.java
+++ src/Test.java
@@ -1,63 +1,89 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Iterator;
-
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.DataNode;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.nodes.TextNode;
+import java.util.Properties;
 
 import nl.siegmann.epublib.domain.Book;
 import nl.siegmann.epublib.domain.Resource;
 import nl.siegmann.epublib.domain.SpineReference;
 import nl.siegmann.epublib.epub.EpubReader;
 
-public class Test {
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.TextNode;
 
-	public static void main(String[] args) {
+public class Test
+{
+
+	public static void main(String[] args)
+	{
 		EpubReader epubReader = new EpubReader();
+		Properties libriMap = new Properties();
+		try
+		{
+			libriMap.load(new FileReader("libri.map"));
+		} catch (FileNotFoundException e1)
+		{
+			e1.printStackTrace();
+		} catch (IOException e1)
+		{
+			e1.printStackTrace();
+		}
 		URL url;
-		try {
-			url = new URL("http://www.verbumweb.net/it/bibbia/Salmi.epub");
+		try
+		{
+			url = new URL("http://www.verbumweb.net/it/bibbia/Genesi.epub");
 			InputStream in = url.openStream();
 			Book book = epubReader.readEpub(in);
-			System.out.println(book.getTitle());
-			Iterator<SpineReference> ir = book.getSpine().getSpineReferences().iterator();
-			while (ir.hasNext()) {
+			String title = book.getTitle();
+			System.out.println(title);
+			title = libriMap.getProperty(title);
+			Iterator<SpineReference> ir;
+			ir = book.getSpine().getSpineReferences().iterator();
+			while (ir.hasNext())
+			{
 				Resource res = ir.next().getResource();
-				System.out.println(res.getTitle());
+				System.out.println(res.getHref());
 				String text = new String(res.getData());
 				Document doc = Jsoup.parse(text);
-				Iterator<Element> parIter;
-				parIter = doc.body().getElementsByTag("p").iterator();
-				while (parIter.hasNext()) {
-					Element para = parIter.next();
-					Iterator<Element> vNumbIter = para.getElementsByTag("span")
-							.iterator();
-					Iterator<TextNode> vTextIter = para.textNodes().iterator();
-					// while (vTextIter.hasNext()) {
-					// TextNode vTextNode = vTextIter.next();
-					// System.out.println(vTextNode.getWholeText());
-					// }
-					// while (vNumbIter.hasNext()) {
-					// String vNumb = vNumbIter.next().text();
-					// System.out.println("[" + vNumb + "]");
-					// }
-					while (vTextIter.hasNext() && vNumbIter.hasNext()) {
-						TextNode vTextNode = vTextIter.next();
-						String vNumb = vNumbIter.next().text();
-						System.out.println("[" + vNumb + "] "
-								+ vTextNode.getWholeText());
+				String chapter = doc.title();
+				if (chapter.startsWith("Cap."))
+				{
+					chapter = chapter.substring(5);
+					Iterator<Element> parIter;
+					parIter = doc.body().getElementsByTag("p").iterator();
+					while (parIter.hasNext())
+					{
+						Element para = parIter.next();
+						Iterator<Element> vNumbIter;
+						vNumbIter = para.getElementsByTag("span").iterator();
+						Iterator<TextNode> vTextIter = para.textNodes().iterator();
+						while (vTextIter.hasNext() && vNumbIter.hasNext())
+						{
+							String vText = vTextIter.next().getWholeText();
+							String vNumb = vNumbIter.next().text();
+							String stanza = "$$$";
+							stanza += title + ".";
+							stanza += chapter + ".";
+							stanza += vNumb + "\n";
+							stanza += vText;
+							System.out.println(stanza);
+						}
 					}
 				}
 			}
-		} catch (MalformedURLException e) {
+		} catch (MalformedURLException e)
+		{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} catch (IOException e) {
+		} catch (IOException e)
+		{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
