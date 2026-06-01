30de404d760b6702b28a3c03a1faf14ea558d42e
bnc-rm <bnc-rm.ict@cultura.gov.it>
2026-06-01 22:27:26 +0200

    Encoding
    - risolti problemi di encoding che causavano caratteri spazzatura
    - riformattato quasi tutto il codice
    - scaricato e aggiunto lo schema XSD OSIS

--- src/TestTilcBible.java
+++ src/TestTilcBible.java
@@ -1,6 +1,9 @@
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
 
 import org.jdom2.output.Format;
 import org.jdom2.output.XMLOutputter;
@@ -10,27 +13,31 @@ import bible.Bible;
 public class TestTilcBible
 {
 
-	public static void main(String[] args) throws IOException
-	{
-		Bible bible = new Bible();
-		if(bible.isToDownLoad())
-		{
-			bible.download();
-		}
-		else
-		{
-			bible.load();
-			if(args[0].equals("osis"))
-			{
-				XMLOutputter xo = new XMLOutputter(Format.getPrettyFormat());
-				xo.output(bible.toOsis(), new FileWriter("cei2008.osis"));
-			}
-			if(args[0].equals("imp"))
-			{
-				PrintWriter pw = new PrintWriter("cei2008.imp");
-				pw.println(bible.toImp());
-				pw.close();
-			}
-		}
-	}
+  public static void main(String[] args) throws IOException
+  {
+    Bible bible = new Bible();
+    if(bible.isToDownLoad())
+    {
+      bible.download();
+    }
+    else
+    {
+      bible.load();
+      if(args[0].equals("osis"))
+      {
+        Format format = Format.getPrettyFormat();
+        format.setEncoding("UTF-8");
+        XMLOutputter xo = new XMLOutputter(format);
+//				xo.output(bible.toOsis(), new FileWriter("cei2008.osis"));
+        xo.output(bible.toOsis(), new OutputStreamWriter(new FileOutputStream("cei2008.osis"), StandardCharsets.UTF_8));
+
+      }
+      if(args[0].equals("imp"))
+      {
+        PrintWriter pw = new PrintWriter("cei2008.imp");
+        pw.println(bible.toImp());
+        pw.close();
+      }
+    }
+  }
 }
--- src/bible/Verse.java
+++ src/bible/Verse.java
@@ -5,79 +5,79 @@ import org.jdom2.Namespace;
 
 public class Verse
 {
-	private String text;
-	private int number;
-	private String swordAcronym;
-	private int chapterNumber;
+  private String text;
+  private int number;
+  private String swordAcronym;
+  private int chapterNumber;
 
-	public int getChapterNumber()
-	{
-		return chapterNumber;
-	}
+  public int getChapterNumber()
+  {
+    return chapterNumber;
+  }
 
-	public void setChapterNumber(int chapterNumber)
-	{
-		this.chapterNumber = chapterNumber;
-	}
+  public void setChapterNumber(int chapterNumber)
+  {
+    this.chapterNumber = chapterNumber;
+  }
 
-	public String getSwordAcronym()
-	{
-		return swordAcronym;
-	}
+  public String getSwordAcronym()
+  {
+    return swordAcronym;
+  }
 
-	public void setSwordAcronym(String swordAcronym)
-	{
-		this.swordAcronym = swordAcronym;
-	}
+  public void setSwordAcronym(String swordAcronym)
+  {
+    this.swordAcronym = swordAcronym;
+  }
 
-	public Verse(int n)
-	{
-		number = n;
-		text = new String();
-	}
+  public Verse(int n)
+  {
+    number = n;
+    text = new String();
+  }
 
-	public Verse(String t, int n)
-	{
-		text = new String(t);
-		number = n;
-	}
+  public Verse(String t, int n)
+  {
+    text = new String(t);
+    number = n;
+  }
 
-	public String getText()
-	{
-		return text;
-	}
+  public String getText()
+  {
+    return text;
+  }
 
-	public void setText(String t)
-	{
-		text = new String(t);
-	}
+  public void setText(String t)
+  {
+    text = new String(t);
+  }
 
-	public void appendText(String t)
-	{
-		text += t;
-	}
+  public void appendText(String t)
+  {
+    text += t;
+  }
 
-	public int getNumber()
-	{
-		return number;
-	}
+  public int getNumber()
+  {
+    return number;
+  }
 
-	public void clean()
-	{
-		text = text.replaceAll("«", "\"");
-	}
-	
-	public Element toOsis(String swordAcronym, int chapter)
-	{
-		Namespace def = Namespace.getNamespace("http://www.bibletechnologies.net/2003/OSIS/namespace");
-		Element verse = new Element("verse", def);
-		verse.setAttribute("osisID", swordAcronym + "." + chapter + "." + number);
-		verse.setText(text);
-		return verse;
-	}
-	
-	public String toImp(String swordAcronym, int chapter)
-	{
-		return "$$$" + swordAcronym + " " + chapter + ":" + number + "\n" + text + "\n";
-	}
+  public void clean()
+  {
+    text = text.replaceAll("«", "\"");
+  }
+
+  public Element toOsis(String swordAcronym, int chapter)
+  {
+    Namespace def = Namespace.getNamespace("http://www.bibletechnologies.net/2003/OSIS/namespace");
+    Element verse = new Element("verse", def);
+    verse.setAttribute("osisID", swordAcronym + "." + chapter + "." + number);
+    verse.setText(text);
+    return verse;
+  }
+
+  public String toImp(String swordAcronym, int chapter)
+  {
+    return "$$$" + swordAcronym + " " + chapter + ":" + number + "\n" + text + "\n";
+  }
 }
